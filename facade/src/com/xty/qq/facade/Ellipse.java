69cb6df795e632f1eda10dbdc6aae410ebbbf67d
qinqun <nanhuaqq@163.com>
2017-12-29 00:44:34 +0800

    facade implement

---
+++ facade/src/com/xty/qq/facade/Ellipse.java
@@ -0,0 +1,11 @@
+package com.xty.qq.facade;
+
+/**
+ * Created by Administrator on 2017/12/29 0029.
+ */
+public class Ellipse implements Shape{
+    @Override
+    public void draw() {
+        System.out.println("draw ellipse");
+    }
+}
